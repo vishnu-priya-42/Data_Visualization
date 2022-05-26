beb382574bf99d82dc9ae01f0593f3fea983cbbc
vishnu-priya-42 <105497070+vishnu-priya-42@users.noreply.github.com>
2022-05-27 00:37:27 +0530

    Update WebsiteDemo.py

--- website/WebsiteDemo.py
+++ website/WebsiteDemo.py
@@ -1,5 +1,3 @@
-# Required Modules
-#   -----------
 from pathlib import Path
 from PIL import Image
 import streamlit as st
@@ -8,7 +6,7 @@ import requests
 
 st.set_page_config(page_title="Data Analysis",page_icon=":chart_with_upwards_trend:",layout="wide")
 
-#Function for displaying graphical content
+#Function for displaying graphical content(eg: image)
 def Graphical_Content(url) :
     r = requests.get(url)
     if r.status_code != 200 :
@@ -17,9 +15,10 @@ def Graphical_Content(url) :
 
 lottie_coding1 = Graphical_Content("https://assets6.lottiefiles.com/packages/lf20_0yfsb3a1.json")
 
+#displaying website main motive
 with st.container():
     left_column, right_column=st.columns((1,2))
-    with left_column :
+    with right_column :
         st.title("Data Analysis Report")
         st.subheader("Data Analysis on Automotive Industry")
         st.write("")
@@ -31,11 +30,11 @@ with st.container():
         </style>
         """, unsafe_allow_html=True)
         st.markdown('<p class="font">This Website summarises the challenge of data analysis on a sample dataset of Automotive Industry</p>', unsafe_allow_html=True)
-    with right_column:
+    with left_column:
         st_lottie(lottie_coding1,height=300,key="coding")
 
 
-# ---- graphical images
+#displaying summary of website
 with st.container():
     st.write("---")
     st.header("About Project")
@@ -44,23 +43,26 @@ with st.container():
     - The data analysis was done on a dataset cars_engage_2022.csv
     
     - Using the dataset, data was analysed to find out most popular car specification,
-      most popular car type and the power of car based on fuel capapcity
+        most popular car type and the power of car based on fuel capapcity
       
     - Models were developed to predict the future sales
-         1. Simple Linear Regression Model
-         2. Multiple Linear Regression Model
+            1. Simple Linear Regression Model
+            2. Multiple Linear Regression Model
     
     - Click on below link to view visualized data
     
     """
 
-st.write("[Data Visualization](https://share.streamlit.io/vishnu-priya-42/abcd/main/website/Data_Analysis.py)")
+st.write("[Data Visualization](http://localhost:8502/)")
 
-# ---- Contact form Info
+# Contact Info
 with st.container() :
     st.write("---")
     st.header("My details")
     st.markdown('<p class="font">Name: Vishnu Priya</p>', unsafe_allow_html=True)
     st.markdown('<p class="font">Branch: Computer Science and Engineering</p>', unsafe_allow_html=True)
     st.markdown('<p class="font">College: Maturi Venkata Subba Rao Enginneering College</p>', unsafe_allow_html=True)
-    
+    st.markdown('<p class="font">LinkedIn profile: </p>', unsafe_allow_html=True)
+    st.write("[Click Here](www.linkedin.com/in/vishnu-priya-mamidi-43638b22a)")
+    st.markdown('<p class="font">Gihub profile: </p>', unsafe_allow_html=True)
+    st.write("[Click Here](https://github.com/vishnu-priya-42/abcd)")
