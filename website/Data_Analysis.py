78639f1f24d8ab315199e65ab6fdcd3862983fd1
vishnu-priya-42 <105497070+vishnu-priya-42@users.noreply.github.com>
2022-05-28 20:56:28 +0530

    Update Data_Analysis.py

--- website/Data_Analysis.py
+++ website/Data_Analysis.py
@@ -62,5 +62,5 @@ with st.container() :
     st.markdown('<p class="font">College: Maturi Venkata Subba Rao Enginneering College</p>', unsafe_allow_html=True)
     st.markdown('<p class="font">LinkedIn profile: </p>', unsafe_allow_html=True)
     st.write("[Click Here](https://www.linkedin.com/in/vishnu-priya-mamidi-43638b22a)")
-    st.markdown('<p class="font">Gihub profile: </p>', unsafe_allow_html=True)
+    st.markdown('<p class="font">Github profile: </p>', unsafe_allow_html=True)
     st.write("[Click Here](https://github.com/vishnu-priya-42/abcd)")
